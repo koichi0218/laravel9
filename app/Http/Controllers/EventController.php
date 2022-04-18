7de39670cf611b94730e8d01722ff6909a1755ed
koichi <kouiti0218@gmail.com>
2022-04-18 21:26:15 +0900

    events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Event;
 use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
+use Illuminate\Support\Facades\DB;
 
 class EventController extends Controller
 {
@@ -17,7 +18,12 @@ class EventController extends Controller
      //一覧
     public function index()
     {
-        return view('manager.events.index');
+        $events = DB::table('events')
+        ->orderBy('start_date', 'asc')
+        ->paginate(10);
+
+        return view('manager.events.index',
+        compact('events'));
     }
 
     /**
